09496e8d4bb2efd62988c7feaae55d1585421c6c
Md Safiyat Reza <md.reza@snapdeal.com>
2015-10-23 16:31:45 +0530

    Fixing iostat.sh

--- iostat.sh
+++ iostat.sh
@@ -1,6 +1,38 @@
+#!/usr/bin/env bash
+
+
+#
+#
+# read -n 1 -s
+#
+#
+
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+if [ "`which iostat`" == "" ]
+then
+    echo "${bold}sysstat${normal} not installed. Installing it..."
+    if [ -f "/etc/debian_version" ]
+    then
+    #for ubuntu
+        sudo apt-get -y install sysstat
+    elif [ -f "/etc/redhat-release" ]
+    #for fedora
+    then
+        sudo yum -y install sysstat
+    else
+        echo "Suitable linux distro not found. Exiting..."
+        exit
+    fi
+fi
+
 ifs=$IFS
 IFS='
 '
+
+count=0
 
 cur=0
 prev=1
@@ -10,12 +42,21 @@ printf "\033[s"
 while [ $cur != $prev ]
 do
 	prev="cur"
-	sleep 1
+        sleep 1
 	ios=`iostat -m`
 	printf "\033[u"
         printf "\033[J"
 	printf "\033[s"
+        date +%_H:%_M:%_S
 	printf "$ios"
+
+
+#        key=""
+#        timeout 3s read -n1 -s key
+#        count=`echo $count + 1 | bc`
+#        echo "$count $key"
+
+
 	cur=""
 	for i in $ios
 	do

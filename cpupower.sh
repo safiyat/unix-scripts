cc4787f179715066df8b8e6c8f38ae0c00a8b017
Md Safiyat Reza <md.reza@snapdeal.com>
2015-10-26 12:15:23 +0530

    On Divya, added curpos.sh. CPUPOWER works, but is a little stupid.

--- cpupower.sh
+++ cpupower.sh
@@ -1,33 +1,42 @@
-ifs=$IFS
-IFS='
-'
+# ifs=$IFS
+# IFS='
+# '
 
-cur=0
-prev=1
+# cur=0
+# prev=1
+
+# printf "\033[s"
+
+# while [[ $cur != $prev ]]
+# do
+# 	prev="cur"
+# 	sleep 1
+# 	ios="cpupower monitor"
+# 	printf "\033[u"
+#       printf "\033[J"
+# 	printf "\033[s"
+# 	printf "`$ios`"
+# 	cur=""
+# 	for i in $ios
+# 	do
+# 	    IFS=' '
+# 	    cur="$cur $(echo $k | cut -d \  -f 6)"
+# 	    IFS='
+# '
+# 	done
+# done
+# echo
+# IFS=$ifs
 
 printf "\033[s"
 
-while [[ $cur != $prev ]]
+while [ true ]
 do
-	prev="cur"
-	sleep 1
-	ios="cpupower monitor"
-	printf "\033[u"
-        printf "\033[J"
-	printf "\033[s"
-	printf "`$ios`"
-	cur=""
-	for i in $ios
-	do
-#		k=$(echo $i | grep ^sd)
-#		if [ `echo $k | wc -w` -gt 0 ]
-#		then
-			IFS=' '
-			cur="$cur $(echo $k | cut -d \  -f 6)"
-			IFS='
-'
-#		fi
-	done
+    sudo cpupower monitor
+    printf "\033[u"
+    printf "\033[J"
+    printf "\033[s"
+
+    sudo cpupower monitor
+    sleep 1
 done
-echo
-IFS=$ifs

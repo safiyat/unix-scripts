5f97e2d3d26d28f274ea15b434b771121ab299a0
Md Safiyat Reza <md.reza@snapdeal.com>
2015-10-27 16:36:09 +0530

    One way of querying DE and running the command. Making way for another, dirtier method.

--- lock.sh
+++ lock.sh
@@ -1,18 +1,50 @@
-test1="The screensaver is inactive"
-test2=`gnome-screensaver-command -q`
-echo HealthPack deployed.
-while [ $# -gt 0 ]
-do
-	echo -n Here at
-	date
-	sleep 10
-	echo -n Logging out at 
-	date
-	gnome-screensaver-command -l
-	i=`$test2 = $test1`
-	while [ i -ne 0 ]
-	do
-		i=`test $test2 = $test1`
-		echo Locked
-	done
-done
+#!/usr/bin/env bash
+
+#
+#  Fedora:  
+#           GNOME
+#
+#  Kubuntu: dm-tool lock
+#           KDE
+#
+#  Ubuntu:  gnome-screensaver-command -l
+#           Unity
+#
+#  Cinnamon:cinnamon-screensaver-command -l
+#
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+function getDE()
+{
+    if [ "$XDG_CURRENT_DESKTOP" = "" ]
+    then
+        desktop=$(echo "$XDG_DATA_DIRS" | sed 's/.*\(xfce\|kde\|gnome\).*/\1/')
+    else
+        desktop=$XDG_CURRENT_DESKTOP
+    fi
+    desktop=${desktop,,}  # convert to lower case
+    eval "$1=\"$desktop\""
+}
+
+function getLockCommand()
+{
+    getDE deskenv
+    case $deskenv in
+        kde)
+            command="dm-tool lock"
+            ;;
+        unity)
+            command="gnome-screensaver-command -l"
+            ;;
+        gnome)
+            command="gnome-screensaver-command -l"
+            ;;
+    esac
+    eval "$1=\"$command\""
+}
+
+
+getLockCommand command
+echo "$command"

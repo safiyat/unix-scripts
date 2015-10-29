16efe56bdc2e0d3b38823693e66e57667dd0102b
Safiyat Reza <md.reza@snapdeal.com>
2015-10-29 19:14:16 +0530

    Added waitForQ to linewise.

--- linewise.sh
+++ linewise.sh
@@ -1,9 +1,20 @@
 IFS_bak=$IFS
+
+function waitForQ
+{
+    read -n 1 -s -t 0.1 key   # read a single character, with timeout
+    if [[ $key == "q" ]]
+    then
+        break
+    fi
+}
+
 filename=$1
 IFS=$'\n'
 for line in `cat $filename`
 do
 	#Anything here.
+	waitForQ
 done
 
 IFS=$IFS_bak

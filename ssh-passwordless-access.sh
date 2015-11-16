8967aeb844e60558b5034e0de4bcdd653810d169
Safiyat Reza <md.reza@snapdeal.com>
2015-11-16 14:49:48 +0530

    Using inbuilt command now.

--- ssh-passwordless-access.sh
+++ ssh-passwordless-access.sh
@@ -2,4 +2,5 @@ echo -n "Enter remote username: "
 read REMOTEUSER
 echo -n "Enter remote hostname/ip: "
 read REMOTEHOST
-cat .ssh/id_rsa.pub | ssh $REMOTEUSER@$REMOTEHOST 'cat >> .ssh/authorized_keys'
+ssh-copy-id $REMOTEUSER@$REMOTEHOST
+
